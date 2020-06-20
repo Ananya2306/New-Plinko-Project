70a768d77259918d4cbf848bd5cad3d8144d71de
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-06-20 23:10:34 +0530

    Update Plinko.js

--- Plinko.js
+++ Plinko.js
@@ -1,16 +1,19 @@
+// Create class for making plinkos
 class Plinko{
     constructor(x,y){
       var options = {
          isStatic: true
       }
+      // give shape of the plinkos
       this.body = Bodies.circle(x,y,10,options);
       this.radius = this.body.circleRadius;
       World.add(world,this.body);
     }
+    // make display function
     display(){
         var pos = this.body.position;
         fill("yellow");
         ellipseMode(CENTER);
-        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+        ellipse(pos.x,pos.y,this.radius,this.radius);
     }
 }
