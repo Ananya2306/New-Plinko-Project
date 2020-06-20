a63d6356914244b3f05e66336a06551f368c6782
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-06-20 23:13:07 +0530

    Update Particles.js

--- Particles.js
+++ Particles.js
@@ -1,22 +1,23 @@
+// Create class for making particles
 class Particle{
     constructor(x,y,radius){
         var options = {
-            restitution: 0.5,
+            restitution: 0.3,
             friction: 0
         }
+        //give the shape of particles
         this.body = Bodies.circle(x,y,radius,options);
         this.color=color(random(0, 255), random(0, 255), random(0, 255));
         this.radius = radius;
         World.add(world, this.body);
     }
-
+// make display function
     display(){
         var pos = this.body.position;
         push();
         colorMode(HSB);
         noStroke();
         translate(pos.x,pos.y);
-       
         fill(this.color);
         ellipse(0,0,this.radius*2);
         pop();
