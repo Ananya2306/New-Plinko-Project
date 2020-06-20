be70ccf5cadf470670b8841566eef4cbffcd0ef6
~ Ananya ~ <63799268+Ananya2306@users.noreply.github.com>
2020-06-20 23:02:56 +0530

    Update Ground.js

--- Ground.js
+++ Ground.js
@@ -1,13 +1,16 @@
+// Create the class for ground
 class Ground {
   constructor(x,y,width,height){
       var options = {
           isStatic: true,
       }
+      //Give the shape of ground
       this.body = Bodies.rectangle(x,y,width,height, options);
       this.width = width;
       this.height = height;
       World.add(world, this.body);
   }
+  // make display function
   display(){
       var pos = this.body.position;
       push();
